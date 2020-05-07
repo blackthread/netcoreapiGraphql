09118f82e5eb405be36469163c606da89204e87c
Steve Keyen <stevekeyen@gmail.com>
2020-05-07 21:03:57 +0200

    add code

---
+++ Database/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreapiGraphql.Database
+{
+    public class Book
+    {
+            public string Id { get; set; }
+
+            public string Name { get; set; }
+
+            public bool Published { get; set; }
+
+            public string Genre { get; set; }
+
+            public string AuthorId { get; set; }
+
+            public Author Author { get; set; }
+        
+    }
+}
